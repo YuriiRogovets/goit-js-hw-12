039efc3bde17d54576abd7b18cc68f97159ce009
YuriiRogovets <yura.rogovets@gmail.com>
2024-01-30 01:00:17 +0200

    added scroll and updated pagination

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
@@ -10,7 +9,6 @@ const gallery = document.querySelector(".gallery");
 const loaderEl = document.querySelector(".loader");
 const loadMoreBtn = document.querySelector('[data-action="load-more"]');
 const messageFinishGallery = document.querySelector(".finish-loader")
-
 
 const lightboxEl = new SimpleLightbox('.gallery a', {
     captionsData: "alt",
@@ -24,42 +22,47 @@ const queryParams = {
   pageSize: 40,
 }; 
 
+    
 searchForm.addEventListener("submit", handleSerch);
 loadMoreBtn.addEventListener("click", handleLoadMore);
-
 
 async function handleSerch(event) {
     event.preventDefault();
     loaderEl.style.display = "block";
+    loadMoreBtn.classList.add("is-hidden"); //прибираємо кнопку loadMore
+    messageFinishGallery.classList.add("is-hidden"); 
+    
     gallery.innerHTML = "";
 
     const form = event.currentTarget;
     queryParams.query = form.elements.query.value.trim();
-    
+    queryParams.page = 1;
     
     if (!queryParams.query) {
     loaderEl.style.display = "none"; 
-    return 
+    return;
     }
 
     try {
         const resp = await fetchImages(queryParams.query);
         if (resp.data.hits.length === 0) {
-                iziToast.error({
+                
+                return iziToast.error({
                     message: "Sorry, there are no images matching your search query. Please try again!",
                     position: "topRight",
                     backgroundColor: "red",
                     icon: "none",
                 });
-            }
-        createMarkup(resp.data.hits);
-        
-        // console.log(resp.data);
-        // console.log("Ура заработало!");
-        // console.log(resp.data.total);
-        queryParams.maxPage = Math.ceil(resp.data.total / queryParams.pageSize); 
-        
-        // рахуємо і записуємо в обʼєкт максимальну кількість сторінок в нашому запиті, для цього ділимо кількість результатів на кількість обʼєктів, які ми отримуємо за один запит + округляємо догори
+                
+                
+        } else {
+            createMarkup(resp.data.hits);
+            
+            loadMoreBtn.classList.remove("is-hidden"); //показуємо кнопку loadMore
+
+            queryParams.maxPage = Math.ceil(resp.data.total / queryParams.pageSize);      
+           // рахуємо і записуємо в обʼєкт максимальну кількість сторінок в нашому запиті,
+        }        
 
     } catch (err) {
         iziToast.error({
@@ -74,15 +77,14 @@ async function handleSerch(event) {
 
             messageFinishGallery.classList.remove("is-hidden"); // виводимо повідовлення про кінець галереї
 
-        } else {
+             loadMoreBtn.classList.add("is-hidden"); // ховаємо кнопку loadMore
+        } 
 
-            loadMoreBtn.classList.remove("is-hidden"); //показуємо кнопку  load More
-        }
-
-        form.reset(); // зкидуємо поля форми
+        form.reset(); // скидуємо поля форми
         loaderEl.style.display = "none";
         
     }
+    console.log(queryParams.page);
 }
 
 async function fetchImages(query) {
@@ -100,47 +102,47 @@ async function fetchImages(query) {
     page: queryParams.page,
     })
      
-    // const responce = await axios.get(`${BASE_URL}/?${searchParams}`);
-    // console.log(responce);
-
-    //  робимо запит
+        //  робимо запит
      return axios.get(`${BASE_URL}/?${searchParams}`);
     
 }
 
 async function handleLoadMore(event) {
-  queryParams.page += 1; // збільшуємо номер сторінки
-    // перед початком запиту - показуємо лоадер і блокуємо кнопку
+    queryParams.page += 1; // збільшуємо номер сторінки
+    // перед початком запиту - показуємо лоадер і ховаємо кнопку
     loaderEl.style.display = "block"
-    loadMoreBtn.disabled = true;
-    loadMoreBtn.classList.add("is-hidden"); //можна або морозити або ховати
+    loadMoreBtn.classList.add("is-hidden"); //можна або ховати або морозити (loadMoreBtn.disabled = true;)
 
-  try {
-      const respNext = await fetchImages(queryParams.query);
-      // робимо запит на наступну сторінку новин
-      
-     createMarkup(respNext.data.hits);//малюємо розмітку
-  } catch (err) {
-    console.log(err);
-  } finally {
-      
-      // після запиту - ховаємо лоадер і розблоковуємо/показуємо кнопку
-      loaderEl.style.display = "none";
-      loadMoreBtn.disabled = false;
-      loadMoreBtn.classList.remove("is-hidden");
-    // buttonService.enable(refs.loadMoreBtn, refs.preloader);
-
-    // після натискання на кнопку та закінчення запиту перевіряємо, якщо ми зараз знаходимось на останній сторінці - то ховаємо кнопку, видаляємо обробник подій і додаємо повідомлення
-    if (queryParams.page === queryParams.maxPage) {
-        loadMoreBtn.classList.add("is-hidden");
-        loadMoreBtn.removeEventListener("click", handleLoadMore);
-        messageFinishGallery.classList.remove("is-hidden");
+    try {
+        const respNext = await fetchImages(queryParams.query);// робимо запит на наступну сторінку
         
+        createMarkup(respNext.data.hits);//малюємо розмітку
+    } catch (err) {
+        iziToast.error({
+            message: "Oops, server connection error!",
+            position: "topRight",
+            backgroundColor: "red",
+            icon: "none",
+                });
+    } finally {
+        
+        loaderEl.style.display = "none";
+       
+        //  перевіряємо, якщо ми зараз знаходимось на останній сторінці - то ховаємо кнопку і додаємо повідомлення
+        if (queryParams.page === queryParams.maxPage) {
+            loadMoreBtn.classList.add("is-hidden");
+            loadMoreBtn.removeEventListener("click", handleLoadMore);
+            messageFinishGallery.classList.remove("is-hidden");
+            
+        } else {
+            loadMoreBtn.classList.remove("is-hidden");
+            const galleryCard = document.querySelector(".gallery-card").getBoundingClientRect();
+        window.scrollBy(0, galleryCard.height * 2);
+        }
+            
     }
-  }
+     
 }
-
-
 
 function createMarkup(arr) {
     const markup = arr.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) =>     
